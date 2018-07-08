1597a9a64b2b0a17f41e138e8e7a7c35fb6300b4
Avatar SD <sd1995@ukr.net>
2018-07-08 07:58:07 +0300

    reply

--- fw/receiver/receiver.ino
+++ fw/receiver/receiver.ino
@@ -4,40 +4,49 @@
 #include <stdio.h>
 
 RF24           radio(9, 10);                              // Создаём объект radio   для работы с библиотекой RF24, указывая номера выводов nRF24L01+ (CE, CSN)
-int            data[2];                                   // Создаём массив для приёма данных
+char            data[3];                                   // Создаём массив для приёма данных
 
 int serialWrite(char c, FILE *f) {
 Serial.write(c); // а это уже стандартная шняга ардуино, которая будет печатать в com порт
 return 0;
 }
 
-void setup(){
-
-      Serial.begin(115200);
+void setup()
+{
+    Serial.begin(115200);
     Serial.println("by S.D.");
     stdout = stderr = fdevopen(serialWrite, NULL);
     
     delay(1000);
     pinMode(5, OUTPUT);
+    
     radio.begin();                                        // Инициируем работу nRF24L01+
     radio.setChannel(80);                                  // Указываем канал приёма данных (от 0 до 127), 5 - значит приём данных осуществляется на частоте 2,405 ГГц (на одном канале может быть только 1 приёмник и до 6 передатчиков)
-    radio.setDataRate     (RF24_2MBPS);                   // Указываем скорость передачи данных (RF24_250KBPS, RF24_1MBPS, RF24_2MBPS), RF24_1MBPS - 1Мбит/сек
-    radio.setPALevel      (RF24_PA_LOW);                 // Указываем мощность передатчика (RF24_PA_MIN=-18dBm, RF24_PA_LOW=-12dBm, RF24_PA_HIGH=-6dBm, RF24_PA_MAX=0dBm)
-
+    radio.setDataRate     (RF24_250KBPS);                   // Указываем скорость передачи данных (RF24_250KBPS, RF24_1MBPS, RF24_2MBPS), RF24_1MBPS - 1Мбит/сек
+    radio.setPALevel      (RF24_PA_MAX);                 // Указываем мощность передатчика (RF24_PA_MIN=-18dBm, RF24_PA_LOW=-12dBm, RF24_PA_HIGH=-6dBm, RF24_PA_MAX=0dBm)
     
     radio.openReadingPipe (1, 0x1234567890LL);            // Открываем 1 трубу с идентификатором 0x1234567890 для приема данных (на ожном канале может быть открыто до 6 разных труб, которые должны отличаться только последним байтом идентификатора)
-    radio.startListening  ();                             // Включаем приемник, начинаем прослушивать открытую трубу
-//  radio.stopListening   ();                             // Выключаем приёмник, если потребуется передать данные
-radio.printDetails();
+    radio.openWritingPipe (0x1234567891LL);
+    radio.printDetails();
+
+    radio.startListening();
 }
-void loop(){
-    if(radio.available()){                                // Если в буфере имеются принятые данные
-        radio.read(data, sizeof(data)-1);                  // Читаем данные в массив data и указываем сколько байт читать
+void loop()
+{
+    if(radio.available())
+    {                                // Если в буфере имеются принятые данные
+        radio.read(data, sizeof(data));                  // Читаем данные в массив data и указываем сколько байт читать
         Serial.print("data: ");
         Serial.print(data[0]);
         Serial.print(", ");
         Serial.println(data[1]);
         digitalWrite(5, data[1]);
+
+        delay(1);
+        radio.stopListening();
+        sprintf(data, "sd");
+        radio.write(data, sizeof(data));
+        radio.startListening();
         
     } /*else
         Serial.println("No data");*/
--- fw/transmitter/transmitter.ino
+++ fw/transmitter/transmitter.ino
@@ -4,11 +4,11 @@
 #include <stdio.h>
 
 RF24           radio(9, 10);                              // Создаём объект radio для работы с библиотекой RF24, указывая номера выводов nRF24L01+ (CE, CSN)
-int            data[3];                                   // Создаём массив для приёма данных
+char            data[3];                                   // Создаём массив для приёма данных
 
 int serialWrite(char c, FILE *) {
-Serial.write(c); // а это уже стандартная шняга ардуино, которая будет печатать в com порт
-return 0;
+    Serial.write(c); // а это уже стандартная шняга ардуино, которая будет печатать в com порт
+    return 0;
 }
 
 void setup()
@@ -21,24 +21,42 @@ void setup()
   
     radio.begin();                                        // Инициируем работу nRF24L01+
     radio.setChannel(80);                                  // Указываем канал приёма данных (от 0 до 127), 5 - значит приём данных осуществляется на частоте 2,405 ГГц (на одном канале может быть только 1 приёмник и до 6 передатчиков)
-    radio.setDataRate     (RF24_2MBPS);                   // Указываем скорость передачи данных (RF24_250KBPS, RF24_1MBPS, RF24_2MBPS), RF24_1MBPS - 1Мбит/сек
-    radio.setPALevel      (RF24_PA_LOW);                 // Указываем мощность передатчика (RF24_PA_MIN=-18dBm, RF24_PA_LOW=-12dBm, RF24_PA_HIGH=-6dBm, RF24_PA_MAX=0dBm)
+    radio.setDataRate     (RF24_250KBPS);                   // Указываем скорость передачи данных (RF24_250KBPS, RF24_1MBPS, RF24_2MBPS), RF24_1MBPS - 1Мбит/сек
+    radio.setPALevel      (RF24_PA_MAX);                 // Указываем мощность передатчика (RF24_PA_MIN=-18dBm, RF24_PA_LOW=-12dBm, RF24_PA_HIGH=-6dBm, RF24_PA_MAX=0dBm)
     radio.openWritingPipe (0x1234567890LL);               // Открываем трубу с идентификатором 0x1234567890 для передачи данных (на одном канале может быть открыто до 6 разных труб, которые должны отличаться только последним байтом идентификатора)
-
+    radio.openReadingPipe (1, 0x1234567891LL);
     data[2] = 0;
     Serial.println("by S.D.");
     radio.printDetails();
 }
 
-void loop(){
+void loop()
+{
     data[0] = '!';                             // считываем показания Trema слайдера с вывода A1 и записываем их в 0 элемент массива data
     data[1] = digitalRead(5);                           // считываем показания Trema потенциометра с вывода A2 и записываем их в 1 элемент массива data
+    radio.write(data, sizeof(data));                     // отправляем данные из массива data указывая сколько байт массива мы хотим отправить. Отправить данные можно с проверкой их доставки: if( radio.write(&data, sizeof(data)) ){данные приняты приёмником;}else{данные не приняты приёмником;}
+    radio.startListening();
+
     Serial.print(data[1]);
     Serial.println(" <- data, status: ");
     
-    radio.write(data, sizeof(data));                     // отправляем данные из массива data указывая сколько байт массива мы хотим отправить. Отправить данные можно с проверкой их доставки: if( radio.write(&data, sizeof(data)) ){данные приняты приёмником;}else{данные не приняты приёмником;}
-    delay(5);
-//    radio.print_status(radio.get_status());
+    //radio.print_status(radio.get_status());
+    //radio.printDetails();
 
-    radio.printDetails();
+    for(uint16_t i = 0; i< 10; i++)
+    {
+        delay(1);
+        if(radio.available())
+        { 
+            radio.read(data, sizeof(data));
+            if(memcmp(data, "sd", 2))
+                printf("Error reply: %02X:%02X:%02X\n", data[0], data[1], data[2]);
+            else
+                printf("Ok!\n");
+            break;
+        }
+        else if(i == 9)
+            printf("No reply =(\n");
+    }
+    radio.stopListening();
 }
